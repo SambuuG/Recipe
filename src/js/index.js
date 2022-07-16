4106fc9116a99c433a38aa5751f3e2b6721d89fa
SambuuG <bumsambuu1017@gmail.com>
2022-07-16 20:40:27 +0800

    state bolon search controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,26 @@
+require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
-search.doSearch().then((r) => console.log(r));
+const state = {};
+const controlSearch = async () => {
+  //1 webees hailtiin tulhuur ug gargaj awna
+  const query = "pizza";
+
+  if (query) {
+    //2 Shineer hailtiin object uusgej ogno
+    state.search = new Search(query);
+
+    //3 Hailt hiihed zoriulj interface UI beldene
+
+    //4 Hailt guitsetgene
+    await state.search.doSearch();
+
+    //5 Ur dun haruulna
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
